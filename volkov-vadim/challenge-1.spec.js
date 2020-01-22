ca33f694144024f43b94bf5920f955141e57d4bc
Vadim Volkov <triv@me.com>
2020-01-21 23:27:00 -0500

    Merge branch 'master' of /Users/vadihome/Documents/092_Projects_J_JS/qa5-project with conflicts.

---
+++ volkov-vadim/challenge-1.spec.js
@@ -0,0 +1,36 @@
+const { expect } = require('chai');
+
+/*
+Переменная с иминем героя
+
+Description
+
+Создайте переменную `myHero` и присвойте ей любое значение.
+
+Приведем пример, который принципиально похож на то что нужно сделать,
+но адаптируйте его к требованиям задачи:
+```
+const nick = 'Sponge Bob';
+
+```
+*/
+
+// Solution
+const myHero = 'Pavlik Morozoff';
+
+
+// Tests
+
+it('Переменная "myHero"существует и имеет значение', () => {
+  expect(myHero).not.undefined;
+});
+
+it('Переменная "myHero" явлется string', () => {
+  expect(myHero).a('String');
+});
+
+it('Переменная "myHero" содержит значение Pavlik Morozoff', () => {
+  expect(myHero).eq('Pavlik Morozoff');
+});
+
+

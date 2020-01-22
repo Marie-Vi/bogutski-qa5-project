aa61c62eea8eae8995b93d76a1df1156d2c08780
andrewrynkevic <rynkevic7@gmail.com>
2020-01-22 13:29:39 -0500

    Challenges issues resolved

--- rynkevic-andrew/Challenge 2.js
+++ rynkevic-andrew/Challenge 2.js
@@ -15,18 +15,53 @@ const pigOne = "Nif-Nif"
 
 //Solution
 
-const pigOne = 'Nif-Nif';
+const character = 'characterName';
 
 //Tests
 
-it('Переменная `pigOne` существует и ей присвоено значение', () =>  {
+const pigOne = 'Nif-Nif';
+const pigTwo = 'Nuf-Nuf';
+const pigThree = 'Naf-Naf';
+const characters = 'Nif-Nif' + ' ' + 'Nuf-Nuf' + ' ' + 'Naf-Naf'
+
+it('Переменная `pigOne` существует и ей присвоено значение', () => {
     expect(pigOne).not.undefined;
 });
 
-it('Переменная `pigOne` отвечает типу `string`', () =>  {
+it('Переменная `pigTwo` существует и ей присвоено значение', () => {
+    expect(pigTwo).not.undefined;
+});
+
+it('Переменная `pigThree` существует и ей присвоено значение', () => {
+    expect(pigThree).not.undefined;
+});
+
+
+it('Переменная `pigOne` отвечает типу `string`', () => {
     expect(pigOne).a('string');
 });
 
-it('Переменной `pigOne` присвоено значение `Nif-Nif` ', () =>  {
+it('Переменная `pigTwo` отвечает типу `string`', () => {
+    expect(pigTwo).a('string');
+});
+
+it('Переменная `pigThree` отвечает типу `string`', () => {
+    expect(pigThree).a('string');
+});
+
+
+it('Переменной `pigOne` присвоено значение `Nif-Nif` ', () => {
     expect(pigOne).eq('Nif-Nif');
+});
+
+it('Переменной `pigTwo` присвоено значение `Nuf-Nuf` ', () => {
+    expect(pigTwo).eq('Nuf-Nuf');
+});
+
+it('Переменной `pigThree` присвоено значение `Naf-Naf` ', () => {
+    expect(pigThree).eq('Naf-Naf');
+});
+
+it('Создайте переменную `characters` и присвойте ей имена всех вышеупомянутых персонажей (используйте метод конкатенации)',  () => {
+    expect(characters).eq('Nif-Nif' + ' ' + 'Nuf-Nuf' + ' ' + 'Naf-Naf')
 });

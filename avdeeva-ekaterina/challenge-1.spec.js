4c67403801b98f05656ed98ecb89c50e3c84e532
Ekaterina Avdeeva <57123662+KatAvdeeva@users.noreply.github.com>
2020-01-22 16:26:26 -0700

    Challenge -1. String (#258)
    * Chellenge -1. String
    
    * Chellenge No.1. String. Updated
    
    * Chellenge No.1. String. Updated

---
+++ avdeeva-ekaterina/challenge-1.spec.js
@@ -0,0 +1,34 @@
+const { expect } = require('chai');
+/*
+Name
+Переменная с именем книги.
+
+Description
+В 1865 году английский математик Льюис Кэрролл написал знаменитую детскую сказку.
+Создайте переменную `nameOfBook` и присвойте ей значение 'Alice in Wonderland'.
+
+Приведем пример, который принципиально похож на то, что нужно сделать,
+но адаптируйте его к требованиям задачи:
+const nameOfNewspaper = 'The New York Times';
+ */
+
+// Solution
+const nameOfBook = 'Alice in Wonderland';
+
+// Tests
+
+it('Переменная `nameOfBook` существует и имеет значение', () => {
+    expect(nameOfBook).not.undefined;
+});
+
+it('Переменная `nameOfBook` содержит тип данных string',  () => {
+    expect(nameOfBook).a('string');
+});
+
+it('Переменная `nameOfBook` содержит значение `Alice in Wonderland`, () => {
+    expect(nameOfBook).eq('Alice in Wonderland');
+});
+
+
+
+

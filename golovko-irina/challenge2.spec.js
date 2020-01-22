8e8ec83ddb22d35ccdb2b706b8c4d5f00fae57ad
irina08 <irina_ger@yahoo.com>
2020-01-21 22:49:52 -0800

    change mistakes

--- golovko-irina/challenge2.spec.js
+++ golovko-irina/challenge2.spec.js
@@ -8,9 +8,9 @@ const { expect } = require('chai');
 // Description
 // Каков результат возведения в нулевую степень числа 101?
 
-// Создайте переменную `number1` и присвойте ей значение 101.
-// Создайте переменную `power1` и присвойте ей значение 0.
-// Создайте переменную `result` и присвойте ей значение number1 в степени power1.
+// Создайте переменную `number` и присвойте ей значение 101.
+// Создайте переменную `power` и присвойте ей значение 0.
+// Создайте переменную `result` и присвойте ей значение number в степени power.
 
 // Приведем пример, который принципиально похож на то что нужно сделать,
 // но адаптируйте его к требованиям задачи:
@@ -22,35 +22,35 @@ const { expect } = require('chai');
 // ```;
 
 // Solution
-const number1 = 101;
-const power1 = 0;
-const result = number1 ** power1;
+const number = 101;
+const power = 0;
+const result = number ** power;
 
 
 // Tests
 
-it('Переменная `number1` содержит значение', () => {
-    expect(number1).not.undefined;
+it('Переменная `number` содержит значение', () => {
+    expect(number).not.undefined;
 });
 
-it('Переменная `number1` содержит число', () => {
-    expect(number1).a('number');
+it('Переменная `number` содержит число', () => {
+    expect(number).a('number');
 });
 
-it('Переменная `number1` равна 101', () => {
-    expect(number1).eq(101);
+it('Переменная `number` равна 101', () => {
+    expect(number).eq(101);
 });
 
-it('Переменная `power1` содержит значение', () => {
-    expect(power1).not.undefined;
+it('Переменная `power` содержит значение', () => {
+    expect(power).not.undefined;
 });
 
-it('Переменная `power1` содержит число', () => {
-    expect(power1).a('number');
+it('Переменная `power` содержит число', () => {
+    expect(power).a('number');
 });
 
-it('Переменная `power1` равна 0', () => {
-    expect(power1).eq(0);
+it('Переменная `power` равна 0', () => {
+    expect(power).eq(0);
 });
 
 it('Переменная `result` содержит значение', () => {

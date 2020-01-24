12e6c91a1672738d8d095e6eb638285626c84b19
stozhkairyna <stsirasts@gmail.com>
2020-01-24 12:10:10 -0600

    challenge-3.spec.js fixed

--- stozhka-iryna/challenge-3.spec.js
+++ stozhka-iryna/challenge-3.spec.js
@@ -2,35 +2,90 @@ const { expect } = require('chai');
 
 /*
 Name
-Убедись что это Mocha
+Полупериметр треугольника.
 
 Description
-Создавая новый проэкт студентам группы QA18 необходимо установить Mocha.
-Что бы это сделать им необходимо ознакомится с тем что же такое Mocha,
-выяснить как сделать Installation а также убедится что это нужная им программа.
+Если известны все три стороны треугольника a, b, c, то формула полупериметра
+треугольника по трем сторонам легко применится на практике:
+где:
 
-Итак
+p – полупериметр треугольника,
+a, b, c – длины сторон треугольника.
+
+Соответственно полупериметр – это сумма длин всех сторон разделенная на 2.
+p=(a+b+c)/2
+
+Создайте переменные с названиями: `sideA`, `sideB` и `sideC`
+Присвойте им значения: 12, 9 и 11 соответственно.
+Создайте переменную с названием `polyPerimeterOfTriangle`.
+Присвойте ей значение, которое вычисляет полупериметр треугольника.
+
 Приведем пример, который принципиально похож на то что нужно сделать,
 но адаптируйте его к требованиям задачи:
 ```
-const framework = 'Mocha';
-```
+const first = 15;
+const second = 20;
+const third = 10;
+const volumeOfSum = (first + second + third)/2;
 */
 
 // Solution
-const framework = 'Mocha';
+
+const sideA = 12;
+const sideB = 9;
+const sideC = 11;
+
+const polyPerimeterOfTriangle = (sideA + sideB + sideC)/2;
+
 
 // Tests
 
-it('Переменная `framework` существует и содержит значение', () => {
-    expect(framework).not.undefined;
+it('Переменная `sideA` содержит значение', () => {
+    expect(sideA).not.undefined;
 });
 
-it('Переменная `framework` тип данных string', () => {
-    expect(framework).a('string');
+it('Переменная `sideA` содержит тип данных number', () => {
+    expect(sideA).a('number');
 });
 
-it('Переменная `framework` содержит значение `Mocha`', () => {
-    expect(framework).eq('Mocha');
+it('Переменная `sideA` содержит значение 12', () => {
+    expect(sideA).equal(12);
 });
+
+it('Переменная `sideB` содержит значение', () => {
+    expect(sideB).not.undefined;
+});
+
+it('Переменная `sideB` содержит тип данных number', () => {
+    expect(sideB).a('number');
+});
+
+it('Переменная `sideB` содержит значение 9', () => {
+    expect(sideB).equal(9);
+});
+
+it('Переменная `sideC` содержит значение', () => {
+    expect(sideC).not.undefined;
+});
+
+it('Переменная `sideC` содержит тип данных number', () => {
+    expect(sideC).a('number');
+});
+
+it('Переменная `sideC` содержит значение 11', () => {
+    expect(sideC).equal(11);
+});
+
+it('Переменная `polyPerimeterOfTriangle` содержит значение', () => {
+    expect(polyPerimeterOfTriangle).not.undefined;
+});
+
+it('Переменная `polyPerimeterOfTriangle` содержит тип данных number', () => {
+    expect(polyPerimeterOfTriangle).a('number');
+});
+
+it('Переменная `polyPerimeterOfTriangle` содержит значение 16', () => {
+    expect(polyPerimeterOfTriangle).equal(16);
+});
+
 

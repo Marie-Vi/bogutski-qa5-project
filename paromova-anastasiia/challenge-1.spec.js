001f6dce8891849f2bb12f8b4591a0d9aaa97b6f
Anastasiia Paromova <miss.paromova@gmail.com>
2020-01-23 22:59:21 -0800

    Challenge 2. Number

--- paromova-anastasiia/challenge-1.spec.js
+++ paromova-anastasiia/challenge-1.spec.js
@@ -2,55 +2,53 @@ const { expect } = require('chai');
 
 /*
 Name
-Увеличение числа в 2 раза
+Увеличение числа в 10 раз
 
 Description
-Мария получала зарплату 27 долларов в час. Но однажды ей приснилось,
-что ее зарплата удвоена. Какая зарплата ей приснилась?
-Напишите команды, увеличивающую значение переменной в два раза.
+Джон впервые в Вегасе. В первом же казино он сделал ставку $30 и сразу же выиграл в 10 раз больше ставки. Какой был выигрыш ?
 
 Для решения задачи:
-Создайте переменную `num` и присвойте ей любое число от 0 до 100.
-Создайте вторую переменную `numMultTo2`, которая будет использовать
-значение переменной `num` умноженное на 2.
-То есть переменная `numMultTo2` будет в 2 раза больше `num`.
+Создайте переменную `bet` и присвойте ей любое число от 0 до 100.
+Создайте вторую переменную `win`, которая будет использовать
+значение переменной `bet` умноженное на 10.
+То есть переменная `win` будет в 10 раз больше `bet`.
 
 Приведем пример, который принципиально похож на то что нужно сделать,
 но адаптируйте его к требованиям задачи:
 ```
-const before = 4;
+const before = 30;
 const after = before * 5;
 ```
 */
 
 // Solution
-const num = 27;
-const numMultTo2 = num * 2;
+const bet = 30;
+const win = bet * 10;
 
 // Tests
 
-  it('should variable `num` be a number',  () => {
-    expect(num).a('number');
+  it('should variable `bet` be a number',  () => {
+    expect(bet).a('bet');
   });
 
-  it('should variable `num` has a value', () => {
-    expect(num).not.undefined;
+  it('should variable `bet` has a value', () => {
+    expect(bet).not.undefined;
   });
 
-  it('should variable `num` be in range from 0 to 100',  () => {
-    expect(num >= 0 && num <= 100).to.be.true;
+  it('should variable `bet` be in range from 0 to 100',  () => {
+    expect(bet >= 0 && bet <= 100).to.be.true;
   });
 
-  it('should variable `numMultTo2` to be a number',  () => {
-    expect(numMultTo2).a('number');
+  it('should variable `win` to be a number',  () => {
+    expect(win).a('bet');
   });
 
-  it('should variable `numMultTo2` has a value', () => {
-    expect(numMultTo2).not.undefined;
+  it('should variable `win` has a value', () => {
+    expect(win).not.undefined;
   });
 
-  it('should variable `numMultTo2` is greater than `num` in 2 times',  () => {
-    expect(num * 2).equal(numMultTo2);
+  it('should variable `win` is greater than `bet` in 10 times',  () => {
+    expect(bet * 10).equal(win);
   });
 
 

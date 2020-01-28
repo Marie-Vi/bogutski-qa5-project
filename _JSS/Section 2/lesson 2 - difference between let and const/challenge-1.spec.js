2214da1fcdfafd93aa819d6e8c877eac31136f52
Viktor Bogutskii <info@siteograf.com>
2020-01-27 19:04:15 -0800

    upd

--- _JSS/Section 2/lesson 2 - difference between let and const/challenge-1.spec.js
+++ _JSS/Section 2/lesson 2 - difference between let and const/challenge-1.spec.js
@@ -13,21 +13,21 @@ Description
 Создайте константу именем `sitUps` (количество приседаний в день) и присвойте ей значение 50.
 Создайте переменную с именем `days` (количество дней) и присвойте ей произвольное число от 1 до 365.
 Создайте переменную `numberOfSitUps` (общее количество приседаний) и присвойте ей значение произведения
-переменных `sitUps` и  `days`.
+переменных `sitUps` и `days`.
 
 Приведем пример, который принципиально похож на то что нужно сделать,
 но адаптируйте его к требованиям задачи:
 ```
 const pencil = 10;
-let boxes = 140;
-let amount = pencil * boxes;
+const boxes = 140;
+const amount = pencil * boxes;
 ```
 */
 
 // Solution
 const sitUps = 50;
-let days = 15;
-let numberOfSitUps = sitUps * days;
+const days = 15;
+const numberOfSitUps = sitUps * days;
 
 // Tests
 
@@ -52,7 +52,7 @@ it('переменная `days` является числом', () => {
 });
 
 it('значение переменной `days` находится в диапазоне от 1 до 365', () => {
-    expect(days>= 1 && days <=365).true;
+    expect(days >= 1 && days <= 365).true;
 });
 
 it('переменная `numberOfSitUps` существует и имеет значение', () => {
